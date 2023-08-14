a40aa4827b871bd06daa860d7dca60c4ecca3f31
nadanasr432 <nadanm432@gmail.com>
2023-08-14 16:13:48 +0300

    first commit

---
+++ view/error.php
@@ -0,0 +1,9 @@
+<?php
+include ('header.php');
+?>
+<h2>Error</he>
+<p><?= $error ?></p>
+<p> <a href=".">Back to list</a></p>
+<?php 
+include ('footer.php');
+?>
---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment Trscker</title>
+    <link rel="stylesheet" href="view/css/main.min.css">
+</head>
+<body>
+
+</body>
+</html>
